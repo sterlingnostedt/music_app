31c9e1f9b3cfdf8c121b7074291667aed25e7e45
sterlingnostedt <sterling.nostedt@gmail.com>
2016-05-16 19:57:56 +0000

    basic functions

--- app/actions.rb
+++ app/actions.rb
@@ -6,4 +6,27 @@ end
 get '/songs' do
   @songs = Song.all
   erb :'songs/index'
+end 
+
+get '/songs/new' do
+  @song = Song.new
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    url: params[:url],
+    author:  params[:author]
+  )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  erb :'songs/show'
 end
